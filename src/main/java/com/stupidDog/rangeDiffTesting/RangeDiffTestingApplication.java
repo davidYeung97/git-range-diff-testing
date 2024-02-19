c37fbc69a860c11eb0c832d9922b6c0f44ff82ed
davidYeung97 <shan-heng.yeung@connect.polyu.hk>
2024-02-19 12:16:11 +0800

    first commit

---
+++ src/main/java/com/stupidDog/rangeDiffTesting/RangeDiffTestingApplication.java
@@ -0,0 +1,13 @@
+package com.stupidDog.rangeDiffTesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RangeDiffTestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RangeDiffTestingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stupidDog/rangeDiffTesting/RangeDiffTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stupidDog.rangeDiffTesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RangeDiffTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
