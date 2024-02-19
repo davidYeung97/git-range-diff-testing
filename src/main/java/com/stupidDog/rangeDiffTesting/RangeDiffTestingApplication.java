47e4815764022a94c4e026478e04161e9457436a
davidYeung97 <shan-heng.yeung@connect.polyu.hk>
2024-02-19 12:26:46 +0800

    feat change one

--- src/main/java/com/stupidDog/rangeDiffTesting/RangeDiffTestingApplication.java
+++ src/main/java/com/stupidDog/rangeDiffTesting/RangeDiffTestingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RangeDiffTestingApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RangeDiffTestingApplication.class, args);
+		       SpringApplication.run(RangeDiffTestingApplication.class, args);
 	}
 
 }
